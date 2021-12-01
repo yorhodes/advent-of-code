1d2955c945eb2efd8cc3ec319d37eea72b8d1ea6
Yorke Rhodes <yorkerhodesiv@gmail.com>
2021-12-01 11:31:09 -0800

    Complete day 1 part 2

--- 2021/day1/largerthanprev.py
+++ 2021/day1/largerthanprev.py
@@ -1,9 +1,23 @@
 with open('input.txt') as f:
     depths = [int(s) for s in f.read().splitlines()]
+    
+    ## part 1
     prev = float('inf')
     count = 0
     for depth in depths:
         if depth > prev:
             count += 1
         prev = depth
-    print(count)
+    print("part1", count)
+
+    ## part 2
+    windowsize = 3
+    count = 0
+    prev = float('inf')
+    for i in range(len(depths)+1-windowsize):
+        window = depths[i:i+windowsize]
+        curr = sum(window)
+        if curr > prev:
+            count += 1
+        prev = curr
+    print("part2", count)
