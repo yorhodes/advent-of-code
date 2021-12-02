e52fe285528744053956ba3677ca1c6e19711194
Yorke Rhodes <yorkerhodesiv@gmail.com>
2021-12-02 12:58:24 -0800

    Complete day 2 part 1 in rust

---
+++ 2021/day2/main.rs
@@ -0,0 +1,26 @@
+use std::io::{self, Read};
+use std::fs::File;
+
+fn filename_to_string(s: &str) -> io::Result<String> {
+    let mut file = File::open(s)?;
+    let mut s = String::new();
+    file.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn main() {
+    let whole_file = filename_to_string("input.txt").unwrap();
+    let mut horizontal_position = 0;
+    let mut depth = 0;
+    whole_file.lines().for_each(|line| {
+        let (direction_str, amount_str) = line.split_once(' ').unwrap();
+        let amount = amount_str.parse::<u32>().unwrap();
+        match direction_str {
+            "forward" => { horizontal_position += amount; },
+            "up" => { depth -= amount; },
+            "down" => { depth += amount; },
+            _ => {}
+        }
+    });
+    println!("result {}", horizontal_position * depth);
+}
