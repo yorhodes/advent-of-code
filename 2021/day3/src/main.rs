c931473f455de46d738cbd7a9b668090990049d9
Yorke Rhodes <yorkerhodesiv@gmail.com>
2021-12-05 12:49:00 -0800

    Add test input and confirm result

--- 2021/day3/src/main.rs
+++ 2021/day3/src/main.rs
@@ -1,25 +1,53 @@
 fn main() {
-    let num_bits = 12;
-
-    let mut bit_counts = vec![0; num_bits];
-
-    include_str!("../input.txt").lines().for_each(|line| {
-        line.chars().enumerate().for_each(|(i, bit)| {
-            bit_counts[i] += if bit == '1' { 1 } else { -1 };
+    let binary_lines: Vec<&str> = include_str!("../test.txt").lines().collect();
+    let num_bits = binary_lines[0].len();
+    let bit_counts_fn = |lines: &Vec<&str>, indices: Vec<usize>| {
+        let mut bit_counts = vec![0; num_bits];
+        lines.iter().for_each(|line| {
+            let bits = line.as_bytes();
+            indices.iter().for_each(|i| {
+                bit_counts[*i] += if bits[*i] == '1' as u8 { 1 } else { -1 };
+            });
         });
-    });
+        return bit_counts;
+    };
 
-    let gamma_rate = u16::from_str_radix(
-        &bit_counts
-            .iter()
-            .map(|count| (if *count > 0 { '1' } else { '0' }))
-            .collect::<String>(),
-        2,
-    ).expect("binary string");
+    let mut gamma = 0;
+    let mut epsilon = 0;
 
-    let epsilon_rate = !(gamma_rate << 4) >> 4;
+    bit_counts_fn(&binary_lines, (0..num_bits).collect())
+        .iter()
+        .for_each(|count| {
+            gamma <<= 1;
+            epsilon <<= 1;
+            if *count > 0 {
+                gamma += 1;
+            } else {
+                epsilon += 1;
+            }
+        });
 
-    println!("gamma * epsilon {}", gamma_rate as usize * epsilon_rate as usize);
+    println!("gamma * epsilon {}", gamma * epsilon);
 
-    
+    let retain_fn = |candidates: &mut Vec<&str>, invert: bool| {
+        for i in 0..num_bits {
+            if candidates.len() > 1 {
+                let counts = bit_counts_fn(&candidates, vec![i, 1]);
+                let label = if (counts[i] >= 0) ^ invert { 1 } else { 0 };
+                candidates.retain(|candidate| candidate.as_bytes()[i] - '0' as u8 == label);
+            } else {
+                break;
+            }
+        }
+    };
+
+    let mut oxygen_candidates = binary_lines.to_vec();
+    retain_fn(&mut oxygen_candidates, false);
+    let oxygen_rate = usize::from_str_radix(oxygen_candidates[0], 2).expect("binary string");
+
+    let mut c02_candidates = binary_lines.to_vec();
+    retain_fn(&mut c02_candidates, true);
+    let c02_rate = usize::from_str_radix(c02_candidates[0], 2).expect("binary string");
+
+    println!("oxygen * c02 {}", oxygen_rate as usize * c02_rate as usize);
 }
