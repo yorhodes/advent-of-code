025ed20bc466e66692ca0784b3e104c09e1b231b
Yorke Rhodes <yorkerhodesiv@gmail.com>
2021-12-06 21:39:45 -0800

    Complete day 7 with tests

--- 2021/day7/src/main.rs
+++ 2021/day7/src/main.rs
@@ -1,9 +1,41 @@
 fn part_1(input: &str) -> usize {
-    return 0;
+    let positions: Vec<usize> = input
+        .split(",")
+        .map(|s| s.trim().parse::<usize>().unwrap()).collect();
+
+    let mut min_fuel = usize::MAX;
+    for p1 in &positions {
+        let mut fuel = 0;
+        for p2 in &positions {
+            let distance = p1.max(p2) - p1.min(p2);
+            fuel += distance;
+        }
+        min_fuel = min_fuel.min(fuel);
+    }
+
+    return min_fuel;
 }
 
 fn part_2(input: &str) -> usize {
-    return 0;
+    let positions: Vec<usize> = input
+        .split(",")
+        .map(|s| s.trim().parse::<usize>().unwrap()).collect();
+
+    let min_pos = positions.iter().min().expect("min");
+    let max_pos = positions.iter().max().expect("max");
+
+    let mut min_fuel = usize::MAX;
+    for p1 in *min_pos..=*max_pos {
+        let mut fuel = 0;
+        for p2 in &positions {
+            let distance = p1.max(*p2) - p1.min(*p2);
+            let cost = distance * (distance + 1) / 2;
+            fuel += cost;
+        }
+        min_fuel = min_fuel.min(fuel);
+    }
+
+    return min_fuel;
 }
 
 const DATA: &str = include_str!("../input.txt");
@@ -20,11 +52,11 @@ mod tests {
 
     #[test]
     fn test_1() {
-        assert_eq!(part_1(SAMPLE_DATA), 0);
+        assert_eq!(part_1(SAMPLE_DATA), 37);
     }
 
     #[test]
     fn test_2() {
-        assert_eq!(part_2(SAMPLE_DATA), 0);
+        assert_eq!(part_2(SAMPLE_DATA), 168);
     }
 }
