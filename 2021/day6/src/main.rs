9ccd42643274394b19882dfd61fd9f7f4d70f67b
Yorke Rhodes <yorkerhodesiv@gmail.com>
2021-12-06 18:31:36 -0800

    Complete day 6 part 1 (with tests)

---
+++ 2021/day6/src/main.rs
@@ -0,0 +1,48 @@
+fn part_1(input: &str, days: usize) -> usize {
+    let max_timer = 8;
+    let mut age_counts = vec![0; max_timer+1];
+
+    // construct counts of each timer
+    input
+        .split(",")
+        .map(|s| s.trim().parse::<usize>().unwrap())
+        .for_each(|age| age_counts[age] += 1);
+
+    // run simulation
+    for _ in 1..=days {
+        let num_zeroes = age_counts[0];
+        for i in 0..max_timer {
+            age_counts[i] = age_counts[i+1];
+        }
+        age_counts[6] += num_zeroes; // each day a 0 becomes a 6
+        age_counts[max_timer] = num_zeroes; // each day a 0 adds a new 8
+    }
+
+    // sum counts of each timer
+    return age_counts.iter().sum();
+}
+
+const DATA: &str = include_str!("../input.txt");
+
+fn main() {
+    let days = 80;
+    println!("part 1: {}", part_1(DATA, days));
+    // println!("part 2: {}", part_2(DATA));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const SAMPLE_DATA: &str = include_str!("../test.txt");
+
+    #[test]
+    fn test_1() {
+        assert_eq!(part_1(SAMPLE_DATA, 18), 26);
+        assert_eq!(part_1(SAMPLE_DATA, 80), 5934);
+    }
+
+    // #[test]
+    // fn test_2() {
+    //     assert_eq!(part_2(SAMPLE_DATA), 12);
+    // }
+}
