ff9b060742d726867ac4c4f2812580aff773d115
Yorke Rhodes <yorkerhodesiv@gmail.com>
2021-12-06 20:59:19 -0800

    Scaffold day 7

---
+++ 2021/day7/src/main.rs
@@ -0,0 +1,30 @@
+fn part_1(input: &str) -> usize {
+    return 0;
+}
+
+fn part_2(input: &str) -> usize {
+    return 0;
+}
+
+const DATA: &str = include_str!("../input.txt");
+
+fn main() {
+    println!("part 1: {}", part_1(DATA));
+    println!("part 2: {}", part_2(DATA));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const SAMPLE_DATA: &str = include_str!("../test.txt");
+
+    #[test]
+    fn test_1() {
+        assert_eq!(part_1(SAMPLE_DATA), 0);
+    }
+
+    #[test]
+    fn test_2() {
+        assert_eq!(part_2(SAMPLE_DATA), 0);
+    }
+}
