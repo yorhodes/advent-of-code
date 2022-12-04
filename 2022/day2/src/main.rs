9e3d1c542b3aadb8cb03961bdd34fd0dae489d35
Yorke Rhodes <email@yorke.dev>
2022-12-04 16:50:59 -0500

    Day 2 part 1

---
+++ 2022/day2/src/main.rs
@@ -0,0 +1,87 @@
+use core::panic;
+
+#[repr(u32)]
+#[derive(Debug, Copy, Clone)]
+enum Move {
+    ROCK,
+    PAPER,
+    SCISSORS,
+}
+
+fn parse_move(input: char) -> Move {
+    return match input {
+        'A' | 'X' => Move::ROCK,
+        'B' | 'Y' => Move::PAPER,
+        'C' | 'Z' => Move::SCISSORS,
+        _ => panic!("invalid move"),
+    };
+}
+
+#[repr(u32)]
+enum Result {
+    LOSS,
+    DRAW,
+    WIN,
+}
+
+fn result(opponent: Move, player: Move) -> Result {
+    let diff = (3 + opponent as u32 - player as u32) % 3;
+    return match diff {
+        0 => Result::DRAW,
+        1 => Result::LOSS,
+        2 => Result::WIN,
+        _ => panic!("invalid result")
+    };
+}
+
+fn score(opponent: Move, player: Move) -> u32 {
+    let result_score = match result(opponent, player) {
+        Result::LOSS => 0,
+        Result::DRAW => 3,
+        Result::WIN => 6
+    };
+    let move_score = match player {
+        Move::ROCK => 1,
+        Move::PAPER => 2,
+        Move::SCISSORS => 3
+    };
+    return result_score + move_score;
+}
+
+fn part_1(input: &str) -> u32 {
+    return input
+        .lines()
+        .map(|move_string| -> u32 {
+            let opponent = parse_move(move_string.chars().nth(0).unwrap());
+            let player = parse_move(move_string.chars().nth(2).unwrap());
+            return score(opponent, player);
+        })
+        .sum();
+}
+
+fn part_2(_input: &str) -> u32 {
+    return 0;
+}
+
+const DATA: &str = include_str!("../input.txt");
+
+fn main() {
+    println!("part 1: {}", part_1(DATA));
+    println!("part 2: {}", part_2(DATA));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const SAMPLE_DATA: &str = include_str!("../test.txt");
+
+    #[test]
+    fn test_1() {
+        assert_eq!(part_1(SAMPLE_DATA), 15);
+    }
+
+    #[test]
+    fn test_2() {
+        assert_eq!(part_2(SAMPLE_DATA), 45000);
+    }
+}
