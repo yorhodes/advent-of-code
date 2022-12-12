e44332a3fecea3b2f12d2e81c32152774b7ccbe9
Yorke Rhodes <email@yorke.dev>
2022-12-12 18:01:11 -0500

    Give up on day7

--- 2022/day7/src/main.rs
+++ 2022/day7/src/main.rs
@@ -1,12 +1,10 @@
 use std::collections::HashMap;
 
 fn part_1(input: &str) -> u32 {
-    let mut sizes: HashMap<&str, u32> = HashMap::new();
-    let mut children: HashMap<&str, Vec<&str>> = HashMap::new();
-    let mut parent: HashMap<&str, &str> = HashMap::new();
+    let mut sizes: HashMap<String, u32> = HashMap::new();
+    let mut children: HashMap<String, Vec<String>> = HashMap::new();
 
-    let mut curr = "/";
-    let mut absolute_path = curr.to_string();
+    let mut absolute_path = String::from("~");
 
     for section in input.split('$').skip(2) {
         let mut lines = section.lines();
@@ -15,45 +13,43 @@ fn part_1(input: &str) -> u32 {
             for entry in lines {
                 let (left, right) = entry.split_once(' ').unwrap();
 
+                let path = absolute_path.clone();
+                let child_path = format!("{}/{}", path, right);
                 if left == "dir" {
-                    children.entry(curr).or_insert(vec![]).push(right);
-                    parent.insert(right, curr);
+                    children.entry(path).or_insert(vec![]).push(child_path);
                 } else {
                     let size: u32 = left.parse().unwrap();
-                    sizes.entry(curr).and_modify(|f|*f += size).or_insert(size);
+                    sizes.entry(path).and_modify(|f|*f += size).or_insert(size);
                 }
             }
         } else { // " cd"
-            println!("{}", command_line);
             let dir = command_line.split(' ').last().unwrap();
+            let path = &absolute_path;
             if dir == ".." {
-                curr = parent.get(curr).unwrap();
-                absolute_path = absolute_path[0..absolute_path.len() - curr.len()].to_string();
+                absolute_path = path.rsplit_once('/').unwrap().0.to_string();
             } else {
-                curr = dir;
-                absolute_path = format!("{}{}", absolute_path, curr);
-                sizes.insert(curr, 0);
+                absolute_path = format!("{}/{}", path, dir);
             }
-            println!("{}", absolute_path);
         }
     }
 
-
     let mut total_sizes = sizes.clone();
-    let mut keys = parent.keys().collect::<Vec<_>>();
-    keys.sort();
+    let mut keys = sizes.keys().collect::<Vec<_>>();
+
+    // sort by number of slashes
+    keys.sort_by(|a, b| a.matches('/').count().cmp(&b.matches('/').count()));
     keys.reverse();
+    
     for k in keys {
-        let p = parent.get(k).unwrap();
-        let child_size = total_sizes.get(k).unwrap().clone();
+        if k == "~" {
+            break;
+        }
+        let child_size = *total_sizes.get(k).unwrap();
+        let p = k.rsplit_once('/').unwrap().0.to_string();
         total_sizes.entry(p).and_modify(|f|*f += child_size);
     }
 
-    let mut values = total_sizes.values().filter(|v| **v <= 100000).collect::<Vec<_>>();
-    values.sort();
-    values.reverse();
-    
-    return values.iter().take(2).map(|v| **v).sum();
+    return total_sizes.values().filter(|v| **v <= 100000).map(|v| *v).sum();
 }
 
 fn part_2(_input: &str) -> u32 {
